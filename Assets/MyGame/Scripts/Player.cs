09a00ec19c8ae5fd9d943955c1410db864a63963
“hubi7676” <hubert.kreutzer02@htl-salzburg.ac.at>
2021-06-07 10:36:50 +0200

    add CollectablesScript and Started making counter

---
+++ Assets/MyGame/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private int eggCounter;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eggCounter = 0;
+    }
+    void OnTriggerEnter(Collider Egg)
+    {
+        if(Egg.tag == "EasterEgg")
+        {
+            eggCounter++;
+            Destroy(Egg.gameObject);
+            scoreText.text = "Score: " + eggCounter;
+            Debug.Log ("Score:" +eggCounter);
+        }
+    }
+}
